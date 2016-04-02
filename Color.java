495f0774fef0b8de6dfb7949eb2bfd8521bcb849
Toshal Patel <toshaltpatel@gmail.com>
2016-04-02 21:10:48 +0530

    Update Color.java

--- Color.java
+++ Color.java
@@ -4,10 +4,10 @@ public class Color {
 	public static void main(String[]args) {
 		int R=0,G=0,B=0;
 		Scanner in = new Scanner(System.in);
-		int T = in.nextInt();
+		int T = in.nextInt(); // number of test cases
 		for(int j=0;j<T;j++) {
-			int N = in.nextInt();
-			String S = in.next();
+			int N = in.nextInt(); //number of rooms to be colored by RGB such that each room is of same color such that min number of rooms are painted
+			String S = in.next(); //string accepting the colors of rooms
 			String[] c = new String[N];
 			for(int i=0; i<N; i++)
 				c[i] = S.substring(i,i+1);
