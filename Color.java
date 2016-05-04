c2a35139b386670ff1456dfd12b9330b787a1e76
Toshal Patel <toshaltpatel@gmail.com>
2016-05-04 21:56:21 +0530

    Update Color.java

--- Color.java
+++ Color.java
@@ -7,7 +7,10 @@ public class Color {
 		int T = in.nextInt(); // number of test cases
 		for(int j=0;j<T;j++) {
 			int N = in.nextInt(); //number of rooms to be colored by RGB such that each room is of same color such that min number of rooms are painted
-			String S = in.next(); //string accepting the colors of rooms
+			int N = in.nextInt();
+			if(N==0)
+				System.out.println("no rooms?!");
+			String S = in.next();
 			String[] c = new String[N];
 			for(int i=0; i<N; i++)
 				c[i] = S.substring(i,i+1);
@@ -19,14 +22,16 @@ public class Color {
 				if(c1.equalsIgnoreCase("B"))
 					B++;
 			}
-			if(R>G && R>B)
-				System.out.println(G+B);
-			if(G>B && G>R)
-				System.out.println(R+B);
-			if(B>G && B>R)
-				System.out.println(R+G);
-			if(R==G && R==B && B==G)
+			if(c.length==R || c.length==B || c.length==G)
 				System.out.println(0);
+			else if(R>G || R>B)
+				System.out.println(G+B);
+			else if(G>B || G>R)
+				System.out.println(R+B);
+			else if(B>G || B>R)
+				System.out.println(R+G);
+			else if(R==G && R==B && B==G)
+				System.out.println(2*R);
 		}
 	}
 }
