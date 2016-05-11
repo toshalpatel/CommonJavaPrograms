fbce98c0acbfaf2c6fec54ab03e4a798d6d6a28b
Toshal Patel <toshaltpatel@gmail.com>
2016-05-11 21:29:42 +0530

    Update FibonacciQueries.java

--- FibonacciQueries.java
+++ FibonacciQueries.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 public class FibonacciQueries {
-	long f;
+	static long f;
 	static int[] A;
 	static int N;
 	
-	public long fibonacci(int k){
+	public static long fibonacci(int k){
 		long a=0,b=1;
-		for(int i=1;i<k+1;i++){
+		for(int i=0;i<k;i++){
 			f=a+(b);
 			a=b;
 			b=f;
@@ -15,19 +15,38 @@ public class FibonacciQueries {
 		return f;
 	}
 	
-	public long sum(int[] b){
+	public static long sum(int[] b){
 		long s=0;
 		for(int i=0;i<b.length;i++)
 			s=s+b[i];
 		return s;
 	}
 	
+	public static void F(int i, int j){
+		// F function
+	}
+	
+	public static void executeQuery(char Q, int i, int j){
+		if (Q=='Q')
+			F(i,j);
+		else
+			A[i]=j;
+	}
+	
 	public static void main(String[]args){
 		Scanner in = new Scanner(System.in);
 		N = in.nextInt();
+		char Q;
+		int i,j;
+		int M = in.nextInt();//no of queries to be performed
 		A = new int[N];
-		for(int i=0;i<N;i++)
-			A[i]=in.nextInt();
-		
+		for(int l=0;l<N;l++)
+			A[l]=in.nextInt();
+		for(int k=0; k<M; k++){
+			Q = in.next().charAt(0);
+			i = in.nextInt();
+			j = in.nextInt();
+			executeQuery(Q,i,j);
+		}
 	}
 }
