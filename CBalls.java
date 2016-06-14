bd56858b4701308b00608f09f02623b04c820bb3
Toshal Patel <toshaltpatel@gmail.com>
2016-06-14 21:05:14 +0530

    Create CBalls.java

---
+++ CBalls.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class CBalls {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for(int i=0; i<T; i++){
+			String X = in.next();
+			String Y = in.next();
+			int N = X.length();
+		}
+	}
+}
