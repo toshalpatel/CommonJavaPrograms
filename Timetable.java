b49e9c41d83c18cd081585087935bd1446347c0e
Toshal Patel <toshaltpatel@gmail.com>
2016-06-20 12:23:45 +0530

    Update Timetable.java

--- Timetable.java
+++ Timetable.java
@@ -1,29 +1,32 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Timetable {
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
-		
 		int T = in.nextInt();
+		int[] C = new int[T];
 		for(int i=0; i<T; i++){
 			int N = in.nextInt();
-			int[] Ai = null;
-			int[] B = null;
-			int[] A = null;
+			int[] Ai = new int[N+1];
+			int[] B = new int[N+1];
+			int[] A = new int[N+1];
 			Ai[0]=0;
-			for(int j=1; j<=N;j++){
+			for(int j=1; j<=N; j++){
 				Ai[j] = in.nextInt();
 				A[j-1] = Ai[j] - Ai[j-1];
 			}
-			for(int k=0; k<N;k++)
+			for(int k=0; k<N; k++)
 				B[k] = in.nextInt();
 			
+			int complete=0;
 			//check
 			for(int m=0;m<N;m++){
-				if(A[m] == B[m]);
+				if(A[m] >= B[m])
+					complete++;
 			}
-			
+			C[i] = complete;
 		}
+		for(int l : C)
+		System.out.println(l);
 	}
 }
