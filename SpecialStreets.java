b40bbf5f702bce18e3880eb2cc62343546ec46c6
Toshal Patel <toshaltpatel@gmail.com>
2016-06-24 13:13:02 +0530

    Create SpecialStreets.java

---
+++ SpecialStreets.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SpecialStreets {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int K = in.nextInt();
+		int[] A = new int[N+1];
+		for(int i=1; i<=N; i++)
+			A[i] = in.nextInt();
+		
+	}
+}
